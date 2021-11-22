5820dfe8b682cad1ee06ffb11cfc075e8f040e98
konstantinasio <92729968+konstantinasio@users.noreply.github.com>
2021-11-22 19:24:58 +0200

    Add files via upload

---
+++ java/Labyrinth.java
@@ -0,0 +1,27 @@
+
+public class Labyrinth {
+
+	public final static int sizeX = 25;
+ 	public final  static int sizeY = 25;
+    
+     public Labyrinth(){
+     int[][]Cells =Cell.initializeCells(sizeX, sizeY);
+	  printLabyrinth(sizeX, sizeY, Cells);  
+     }
+	
+	
+	 public static void printLabyrinth(int x, int y, int[][] z) {
+		int i = 0;
+		int j = 0;
+		
+		for (i = 0; i < x; i++) {
+			for (j = 0; j < y; j++) {
+				System.out.printf("%d", z[i][j]);
+				
+			}
+			System.out.println();
+		}
+	}
+	
+}
+
