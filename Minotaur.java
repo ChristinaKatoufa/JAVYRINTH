0b69ae67ab9f2800f0ecf7a57e362f785b08b4fa
ChristinaKatoufa <92477961+ChristinaKatoufa@users.noreply.github.com>
2021-11-23 20:22:01 +0200

    Second GameCharacter

---
+++ Minotaur.java
@@ -0,0 +1,22 @@
+
+public class Minotaur implements Characters {
+
+	@Override
+	public void image() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dies() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
