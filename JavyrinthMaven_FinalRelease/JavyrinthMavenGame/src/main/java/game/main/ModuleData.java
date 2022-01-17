5ee51aa51fca29c60bcd99121ee9f1ce2ca6c844
ChristinaKatoufa <92477961+ChristinaKatoufa@users.noreply.github.com>
2022-01-17 18:53:25 +0200

    CHANGED Default Value to -2 and ADDED to comments

--- JavyrinthMaven_FinalRelease/JavyrinthMavenGame/src/main/java/game/main/ModuleData.java
+++ JavyrinthMaven_FinalRelease/JavyrinthMavenGame/src/main/java/game/main/ModuleData.java
@@ -11,8 +11,10 @@ public final class ModuleData {
 	 *  By default even if it hasn't instansiated = 0. 
 	 *  Thus we can distinct the cases between it hasn't been set yet, 
 	 *  and asked to quit game.
+         * if there could be a possibility for a player to win at 0 seconds, we would have a problem
+         * we wouldn't distinguish the states between player winning so fast or no data have dispatched yet.(no data yet)
 	 */
-	private static volatile long gameplayDuration; // By convention if game is at STATE_EXIT this should be -1. By default even if it hasn't instansiated = 0. Thus we can distinct the cases between it hasn't been set yet, and asked to quit game.
+	private static volatile long gameplayDuration = -2;
 	
 	/**
 	 * Forbid the usage of the default constructor from outside the class
